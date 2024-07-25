f6187510e6c79283865cc257743cfa8c80df895b
0x59584C <xueliangwd@gmail.com>
2024-07-25 14:55:50 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "Leon"
 site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
+site_build_date = "2024-7-25T15:00+08:00"
 author = "Leon"
 email = "xueliangwd@163.com"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author_homepage = "https://xueliangwd.github.io/leon"
+description = "懒人随记。懂的都懂。"
+key_words = ['Maverick', 'iOS', 'Leon', 'blog']
 language = 'zh-CN'
 external_links = [
     {
